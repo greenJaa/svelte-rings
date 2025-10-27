691fd958447f6247e8c067ef38521e1a9e94d567
OlegJaa <greenzmey@gmail.com>
2025-10-27 16:10:56 +0200

    a

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+console.log("API Key being used:", import.meta.env.VITE_FIREBASE_API_KEY); // <--- ADD THIS LINE
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
