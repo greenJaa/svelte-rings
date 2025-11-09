812fab1a393f0a7fd6c56d6a5c1f88393e779eba
OlegJaa <greenzmey@gmail.com>
2025-11-09 17:02:37 +0200

    save

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,22 +1,29 @@
-// Import the functions you need from the SDKs you need
+// src/lib/firebase.js
+
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics, isSupported } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBIHef4A_WLXDpC6G9sI5X4ua52O-6f6Cg",
-  authDomain: "svelte-rings.firebaseapp.com",
-  projectId: "svelte-rings",
-  storageBucket: "svelte-rings.firebasestorage.app",
-  messagingSenderId: "935307364369",
-  appId: "1:935307364369:web:0b311edfceda3a4caa89b0",
-  measurementId: "G-226691LZLT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Analytics works only in browsers that support it
+let analytics = null;
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+export { app, analytics };
