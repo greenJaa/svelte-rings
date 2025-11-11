26dfdd4784ed70b7aa3203f74e9fc26c56b9d850
OlegJaa <greenzmey@gmail.com>
2025-11-11 17:42:14 +0200

    sss

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,7 +113,13 @@ resource "aws_security_group" "k8s_sg" {
     protocol  = "tcp"
     self      = true
   }
-
+  ingress {
+    description = "NodePort Range (for deployed apps)"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
